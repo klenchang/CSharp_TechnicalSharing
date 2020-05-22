5f97f89cabb3dd60e7608ce18a297f2fc8b76435
Klen <klenchang0313@gmail.com>
2020-05-22 10:42:55 +0800

    commit signalr sample

---
+++ SampleCode/SignalRSample/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRSample.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            message += $", connection id: {Context.ConnectionId}";
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.Client(Context.ConnectionId).SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
